1499653e4e0aa776580fe1cc5c7a5d15ec13921a
Pham-Van-Khiem-Toan-Tin <phamkhiemhust2001@gmail.com>
2024-03-06 02:12:33 +0700

    change packing

---
+++ src/main/java/com/book/app/controller/UserManagementController.java
@@ -0,0 +1,14 @@
+package com.book.app.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserManagementController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
